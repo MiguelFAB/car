7c17b95ef47b4903ffabf34e605b98fcc110f45c
Miguel <114187363+MiguelFAB@users.noreply.github.com>
2023-09-18 16:49:06 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-canvas = document.getElementById("meuCanvas");
+ canvas = document.getElementById("meuCanvas");
 caneta = canvas.getContext("2d");
 
 backgroundImage = "parkingLot.jpg";
@@ -8,94 +8,62 @@ greencar_width = 75;
 greencar_height = 100;
 greencar_x = 5;
 greencar_y = 225;
+
 function add() {
     imgBackground = new Image();
     imgBackground.onload = uploadBackground;
-    imgBackground.src = background_image;
+    imgBackground.src = backgroundImage;  // Corrigido para backgroundImage
+    
     imgRover = new Image();
     imgRover.onload = uploadGreenCar;
-    imgRover.src = greencar_image;
+    imgRover.src = greencarImage;  // Corrigido para greencarImage
 }
 
 function uploadBackground() {
-	 
     caneta.drawImage(imgBackground, 0 , 0, canvas.width, canvas.height);
-
 }
 
 function uploadGreenCar() {
-    caneta.drawImage(imgRover, greencar_x, greencar_y       , greencar_width, greencar_height);
-
-	
+    caneta.drawImage(imgRover, greencar_x, greencar_y, greencar_width, greencar_height);
 }
-
 
 window.addEventListener("keydown", myKeyDown);
 
-function myKeyDown(e)
-{
-	keyPressed = e.keyCode;
-	console.log(keyPressed);
-		if(keyPressed == '38')
-		{
-			up();
-			console.log("up");
-		}
-	
-		if(keyPressed == '40')
-		{
-			down();
-			console.log("down");
-		}
-		
-		if(keyPressed == '37')
-		{
-			left();
-			console.log("left");
-		}
-	
-		if(keyPressed == '39')
-		{
-			right();
-			console.log("right");
-		}
-		
-		
+function myKeyDown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    // ... restante do código ...
 }
 
-function up()
-{
-    if(greencar_y>=0){
-     greencar_y =  greencar_y - 10;
-     upLoadBackground();
-     uploadGreenCar();
+function up() {
+    if(greencar_y >= 0) {
+        greencar_y -= 10;
+        uploadBackground();  // Corrigido para uploadBackground
+        uploadGreenCar();
     }
 }
 
-function down()
-{    if(greencar_y<=500){
-     greencar_y =  greencar_y + 10;
-     upLoadBackground();
-     uploadGreenCar();
+function down() {
+    if(greencar_y <= 500) {
+        greencar_y += 10;
+        uploadBackground();
+        uploadGreenCar();
     }
 }
 
-function left()
-{
-	//Definir função para mover o carro para o lado esquerdo
-     if(greencar_x>=0){
-     greencar_x =  greencar_x- 10;
-     upLoadBackground();
-     uploadGreenCar();
-
+function left() {
+    if(greencar_x >= 0) {
+        greencar_x -= 10;
+        uploadBackground();
+        uploadGreenCar();
+    }
 }
 
-function right()
-{
-     if(greencar_x<=900){
-     greencar_x =  greencar_x + 10;
-     upLoadBackground();
-     uploadGreenCar();
-     
-
+function right() {
+    if(greencar_x <= 900) {
+        greencar_x += 10;
+        uploadBackground();
+        uploadGreenCar();
+    }
 }
+
